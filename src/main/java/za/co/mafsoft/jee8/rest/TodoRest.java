371304b29d86c8b6e74a1ddcc585022ac3cbfa40
Ian Ngulube <ngulubeian94@gmail.com>
2026-02-18 12:56:09 +0200

    cleanup

--- src/main/java/za/co/mafsoft/jee8/rest/TodoRest.java
+++ src/main/java/za/co/mafsoft/jee8/rest/TodoRest.java
@@ -9,6 +9,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import lombok.NoArgsConstructor;
 import za.co.mafsoft.jee8.entity.Todo;
 import za.co.mafsoft.jee8.service.TodoService;
 
@@ -19,42 +20,48 @@ import jakarta.ws.rs.core.Response;
 @Path("todo")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@NoArgsConstructor(force = true)
 public class TodoRest {
 
     private final TodoService todoService;
 
     @Inject
-    public TodoRest(TodoService todoService) {
+    public TodoRest(final TodoService todoService) {
         this.todoService = todoService;
     }
 
     @POST
     @Path("new")
     public Response createTodo(@Valid Todo todo) {
+        assert todoService != null;
         return Response.ok(todoService.createTodo(todo)).build();
     }
 
     @POST
     @Path("edit")
     public Response editTodo(Todo todo) {
+        assert todoService != null;
         return Response.ok(todoService.updateTodo(todo)).build();
     }
 
     @GET
     @Path("{id}")
     public Response getTodo(@PathParam("id") Long id) {
+        assert todoService != null;
         return Response.ok(todoService.findTodo(id)).build();
     }
 
     @GET
     @Path("list")
     public Response getTodos() {
+        assert todoService != null;
         return Response.ok(todoService.getTodos()).build();
     }
 
     @PUT
     @Path("complete")
     public Response markAsComplete(@QueryParam("id") Long id) {
+        assert todoService != null;
         Todo todo = todoService.findTodo(id);
         todo.setCompleted(true);
         return Response.ok(todoService.updateTodo(todo)).build();
